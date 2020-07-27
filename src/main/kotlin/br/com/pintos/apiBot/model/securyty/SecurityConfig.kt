8f2f9561c457d72ba2fd738cb6910a86b82e2f10
ivaneyvieira <ivaneyvieira@gmail.com>
2020-07-27 11:48:55 -0300

    Redireciona

--- src/main/kotlin/br/com/pintos/apiBot/model/securyty/SecurityConfig.kt
+++ src/main/kotlin/br/com/pintos/apiBot/model/securyty/SecurityConfig.kt
@@ -1,6 +1,12 @@
 package br.com.pintos.apiBot.model.securyty
 
 import br.com.pintos.apiBot.model.utils.SaciPasswordEncoder
+import org.apache.catalina.Context
+import org.apache.catalina.connector.Connector
+import org.apache.tomcat.util.descriptor.web.SecurityCollection
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
@@ -47,4 +53,29 @@ class SecurityConfig(val userSaciDetailsService: UserSaciDetailsService): WebSec
   
   @Bean
   fun encoder(): PasswordEncoder = SaciPasswordEncoder()
+  
+  @Bean
+  fun servletContainer(): ServletWebServerFactory? {
+    val tomcat: TomcatServletWebServerFactory = object: TomcatServletWebServerFactory() {
+      override fun postProcessContext(context: Context) {
+        val securityConstraint = SecurityConstraint()
+        securityConstraint.userConstraint = "CONFIDENTIAL"
+        val collection = SecurityCollection()
+        collection.addPattern("/*")
+        securityConstraint.addCollection(collection)
+        context.addConstraint(securityConstraint)
+      }
+    }
+    tomcat.addAdditionalTomcatConnectors(getHttpConnector())
+    return tomcat
+  }
+  
+  private fun getHttpConnector(): Connector? {
+    val connector = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
+    connector.scheme = "http"
+    connector.port = 8080
+    connector.secure = false
+    connector.redirectPort = 8443
+    return connector
+  }
 }
