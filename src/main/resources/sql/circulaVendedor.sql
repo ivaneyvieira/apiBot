41a10189f9ee106ea1729fedd26634d9f4bd0e3a
ivaneyvieira <ivaneyvieira@gmail.com>
2020-07-27 15:28:12 -0300

    Remove o banco bi

--- src/main/resources/sql/circulaVendedor.sql
+++ src/main/resources/sql/circulaVendedor.sql
@@ -1,15 +1,15 @@
 DO @LISTA := :lista;
 
 DO @DATATIME := (SELECT MIN(ultimoAcesso)
-		 FROM bi.televenda
+		 FROM televenda
 		 WHERE lista = @LISTA);
 
 DO @NUMERO := (SELECT MAX(numero)
-	       FROM bi.televenda
+	       FROM televenda
 	       WHERE lista = @LISTA
 		 AND ultimoAcesso = @DATATIME);
 
-UPDATE bi.televenda
+UPDATE televenda
 SET ultimoAcesso = current_timestamp
 WHERE numero = @NUMERO;
 
@@ -20,6 +20,6 @@ SELECT numero,
        cast(CONCAT('https://api.whatsapp.com/send?phone=55',
 		   replace(replace(replace(replace(celular, ' ', ''), '(', ''), ')', ''), '-',
 			   '')) AS CHAR) AS link
-FROM bi.televenda
+FROM televenda
 WHERE lista = @LISTA
   AND numero = @NUMERO;
