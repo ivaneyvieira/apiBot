ab1a88a3f26c0a3e42c21907572ae31b4abd6755
ivaneyvieira <ivaneyvieira@gmail.com>
2020-07-27 11:54:53 -0300

    Remove redirect

--- src/main/kotlin/br/com/pintos/apiBot/model/securyty/SecurityConfig.kt
+++ src/main/kotlin/br/com/pintos/apiBot/model/securyty/SecurityConfig.kt
@@ -54,28 +54,5 @@ class SecurityConfig(val userSaciDetailsService: UserSaciDetailsService): WebSec
   @Bean
   fun encoder(): PasswordEncoder = SaciPasswordEncoder()
   
-  @Bean
-  fun servletContainer(): ServletWebServerFactory? {
-    val tomcat: TomcatServletWebServerFactory = object: TomcatServletWebServerFactory() {
-      override fun postProcessContext(context: Context) {
-        val securityConstraint = SecurityConstraint()
-        securityConstraint.userConstraint = "CONFIDENTIAL"
-        val collection = SecurityCollection()
-        collection.addPattern("/*")
-        securityConstraint.addCollection(collection)
-        context.addConstraint(securityConstraint)
-      }
-    }
-    tomcat.addAdditionalTomcatConnectors(getHttpConnector())
-    return tomcat
-  }
-  
-  private fun getHttpConnector(): Connector? {
-    val connector = Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
-    connector.scheme = "http"
-    connector.port = 8080
-    connector.secure = false
-    connector.redirectPort = 8443
-    return connector
-  }
+
 }
